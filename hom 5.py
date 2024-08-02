0743290d78705f761ac9a7abb19684d00a7d7c0b
AnetoliyBoltovski <tolikboltovskij11@gmail.com>
2024-08-02 15:42:12 +0300

    Update hom 5.py

--- hom 5.py
+++ hom 5.py
@@ -1,21 +1,21 @@
-function
-longest_common_increasing_subsequence(arr1, arr2):
-n = len(arr1)
-m = len(arr2)
+def longest_common_increasing_subsequence(arr1, arr2):
+    n = len(arr1)
+    m = len(arr2)
 
-dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    dp = [0] * (m + 1)
+    for i in range(n):
+        current = 0
+        for j in range(m):
+            if arr1[i] == arr2[j]:
+                if current + 1 > dp[j + 1]:
+                    dp[j + 1] = current + 1
+            elif arr1[i] > arr2[j]:
+                if dp[j + 1] > current:
+                    current = dp[j + 1]
 
-for i from 1 to n:
-    for j from 1 to m:
-        if arr1[i - 1] == arr2[j - 1]:
-            dp[i][j] = 1 + max(dp[x][j - 1] for x
-            from
+    return max(dp)
 
-            1
-            to
-            i - 1 if arr1[x - 1] < arr1[i - 1])
-            else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-result = max(max(row) for row in dp)
-return result
+arr1 = [1, 3, 4, 1, 2, 3]
+arr2 = [3, 4, 1, 2, 1, 3]
+print(longest_common_increasing_subsequence(arr1, arr2))
